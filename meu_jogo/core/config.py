b8dc84ca248beaf08b24081033e79b1f1a6a5774
Vinicius-Oliveira-R <viniciusoliveira12353@gmail.com>
2026-02-28 00:20:54 -0300

    feat: incrementa entrada de teclado

--- meu_jogo/core/config.py
+++ meu_jogo/core/config.py
@@ -1,5 +1,7 @@
 import pygame
-
+x = 400
+y = 300
+velocidade = 50
 pygame.init()
 
 janela = pygame.display.set_mode((800,600))
@@ -8,9 +10,27 @@ pygame.display.set_caption('Elmental-Battle')
 janela_aberta = True
 
 while janela_aberta:
+    pygame.time.delay(50)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             janela_aberta = False
+
+
+    comandos = pygame.key.get_pressed()
+    if comandos[pygame.K_UP]:
+        y-=velocidade
+    if comandos[pygame.K_DOWN]:
+        y+=velocidade
+    if comandos[pygame.K_LEFT]:
+        x-=velocidade
+    if comandos[pygame.K_RIGHT]:
+        x+=velocidade
+        
+    janela.fill((0,0,0))
+
+    pygame.draw.circle(janela,(0,255,0),(x,y),50)
+    pygame.display.update()
 
 
 pygame.quit()
