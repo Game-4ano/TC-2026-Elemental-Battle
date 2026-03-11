3126a44fb0db332e855eccf3c0b38c361b07eb0a
arturflacon <flaconartur@gmail.com>
2026-03-11 20:54:29 -0300

    feat: mapa

--- meu_jogo/data/maps_data.py
+++ meu_jogo/data/maps_data.py
@@ -1,6 +1,7 @@
-from meu_jogo.core.map import GameMap
+from meu_jogo.core.map import Tile, GameMap, GrassTile, WaterTile, FireTile, WindTile, WallTile
 from meu_jogo.data.characters_data import slime, goblin, wolf, forest_guardian
 
+# Dados de mapas antigos (para compatibilidade)
 map1 = GameMap(
     "Floresta",
     enemies=[slime, goblin, wolf],
@@ -8,3 +9,32 @@ map1 = GameMap(
 )
 
 maps = [map1]
+
+# Definição dos tipos de tiles usando Polimorfismo
+TILE_TYPES = {
+    "G": GrassTile(),
+    "P": WaterTile(),
+    "B": FireTile(),
+    "V": WindTile(),
+    "X": WallTile(),
+    " ": GrassTile(), # Vazio como grama por padrão
+}
+
+# Matriz de mapa maior (20x15) com biomas elementais
+MAP_MATRIX = [
+    ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+    ["X", "G", "G", "G", "G", "X", "V", "V", "V", "V", "V", "V", "V", "V", "X", "G", "G", "G", "G", "X"],
+    ["X", "G", "G", "G", "G", "X", "V", "V", "V", "V", "V", "V", "V", "V", "X", "G", "G", "G", "G", "X"],
+    ["X", "G", "G", "G", "G", "X", "X", "X", "V", "V", "X", "X", "X", "X", "X", "G", "G", "G", "G", "X"],
+    ["X", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "X"],
+    ["X", "X", "X", "G", "G", "X", "X", "X", "X", "X", "X", "X", "X", "G", "G", "X", "X", "X", "X", "X"],
+    ["X", "P", "P", "G", "G", "P", "P", "P", "P", "P", "P", "P", "P", "G", "G", "B", "B", "B", "B", "X"],
+    ["X", "P", "P", "G", "G", "P", "P", "P", "P", "P", "P", "P", "P", "G", "G", "B", "B", "B", "B", "X"],
+    ["X", "P", "P", "G", "G", "P", "P", "P", "P", "P", "P", "P", "P", "G", "G", "B", "B", "B", "B", "X"],
+    ["X", "X", "X", "G", "G", "X", "X", "X", "X", "X", "X", "X", "X", "G", "G", "X", "X", "X", "X", "X"],
+    ["X", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", "X"],
+    ["X", "G", "G", "G", "G", "X", "X", "X", "B", "B", "X", "X", "X", "X", "X", "G", "G", "G", "G", "X"],
+    ["X", "G", "G", "G", "G", "X", "B", "B", "B", "B", "B", "B", "B", "B", "X", "G", "G", "G", "G", "X"],
+    ["X", "G", "G", "G", "G", "X", "B", "B", "B", "B", "B", "B", "B", "B", "X", "G", "G", "G", "G", "X"],
+    ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+]
